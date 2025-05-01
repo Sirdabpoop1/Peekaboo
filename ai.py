c77d22ec8125adac4f2bd01855eb2697fb4c81b8
Sirdabpoop1 <adrian.pun27@stgeorges.bc.ca>
2025-05-01 12:42:44 -0700

    s

--- ai.py
+++ ai.py
@@ -1,6 +1,7 @@
 #Dependencies
 import cv2
 import os
+import time
 import random
 import numpy as np
 import uuid
@@ -19,47 +20,20 @@ for gpu in gpus:
     print(gpu)
     tf.config.experimental.set_memory_growth(gpu, True)
 
+IMAGES_PATH = os.path.join("data", "images")
+number_images = 30
 
-
-
-
-
-
-
-
-#Setup paths
-POS_PATH = os.path.join("data", "positive")
-NEG_PATH = os.path.join("data", "negative")
-ANC_PATH = os.path.join("data", "anchor")
-
-#Making folders
-os.makedirs(POS_PATH, exist_ok=True)
-os.makedirs(NEG_PATH, exist_ok=True)
-os.makedirs(ANC_PATH, exist_ok=True)
-
-#Sets up camera
 cam = cv2.VideoCapture(0)
-while cam.isOpened():
+for imgnum in range(number_images):
+    print("Collecting Image #{}".format(imgnum))
     ret, frame = cam.read()
+    imgname = os.path.join(IMAGES_PATH, f"{str(uuid.uuid1())}.jpg")
+    cv2.imwrite(imgname, frame)
+    cv2.imshow("frame", frame)
+    time.sleep(0.5)
 
-    #Cut down the frame to 250px x 250px
-    frame = frame[120:120+250, 200:200+250, :]
-
-    #Take anchors
-    if cv2.waitKey(1) & 0XFF == ord("a"):
-        imgname = os.path.join(ANC_PATH, "{}.jpg".format(uuid.uuid1()))
-        cv2.imwrite(imgname, frame)
-    #Take positives
-    if cv2.waitKey(1) & 0XFF == ord("p"):
-        imgname = os.path.join(POS_PATH, "{}.jpg".format(uuid.uuid1()))
-        cv2.imwrite(imgname, frame)
-
-    cv2.imshow("Camera", frame)
-    
-    if cv2.waitKey(1) & 0XFF == ord("q"):
+    if cv2.waitKey(1) & 0XFF == ord('q'):
         break
 
-#Releases the webcam
-cam.release()
-#Closes the video
+cv2.release()
 cv2.destroyAllWindows()
