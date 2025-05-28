1908407db19e9958cf59588fd8022fa4e1bac298
Sirdabpoop1 <adrian.pun27@stgeorges.bc.ca>
2025-05-28 13:45:44 -0700

    s

--- augmentor.py
+++ augmentor.py
@@ -21,7 +21,7 @@ for partition in ['train', 'test', 'val']:
         img = cv2.imread(os.path.join('data', partition, 'images', image))
         
         coords = [0, 0, 0.00001, 0.00001]
-        label_path = os.path.join('data', partition, 'labels', f'{image.split('.')[0]}.json')
+        label_path = os.path.join('data', partition, 'labels', f'{image.split(".")[0]}.json')
         if os.path.exists(label_path):
             with open(label_path, 'r') as f:
                 label = json.load(f)
@@ -35,28 +35,27 @@ for partition in ['train', 'test', 'val']:
         try:
             for x in range(60):
                 augmented = augmentor(image = img, bboxes = [coords], class_labels = ['face'])
-                cv2.imwrite(os.path.join('aug'))
-                cv2.rectangle(augmented['image'],
+                cv2.imwrite(os.path.join('aug_data', partition, 'images', f'{image.split(".")[0]}.{x}.jpg'), augmented['image'])
+                
+                annotation = {}
+                annotation['image'] = image
+
+                if os.path.exists(label_path):
+                    if len(augmented['bboxes']) == 0:
+                        annotation['bbox'] = [0, 0, 0, 0]
+                        annotation['class'] = 0
+                    else:
+                        annotation['bbox'] = augmented['bboxes'][0]
+                        annotation['class'] = 1
+                else:
+                    annotation['bbox'] = [0, 0, 0, 0]
+                    annotation['class'] = 0
 
 
+                with open(os.path.join('aug_data', partition, 'labels', f'{image.split(".")[0]}.{x}.json'), 'w') as f:
+                    json.dump(annotation, f)
 
+                print("Made an image")
 
-img = cv2.imread(os.path.join('data', 'train', 'images', '8a1c45f7-26d5-11f0-8723-b81ea44562d0.jpg'))
-
-with open(os.path.join('data', 'train', 'labels', '8a1c45f7-26d5-11f0-8723-b81ea44562d0.json'), 'r') as f:
-    label = json.load(f)
-    coords = [0, 0, 0, 0]
-    coords[0] = label['shapes'][0]['points'][0][0]
-    coords[1] = label['shapes'][0]['points'][0][1]
-    coords[2] = label['shapes'][0]['points'][1][0]
-    coords[3] = label['shapes'][0]['points'][1][1]
-    coords = list(np.divide(coords, [640, 480, 640, 480]))
-    augmented = augmentor(image = img, bboxes = [coords], class_labels = ['face'])
-    cv2.rectangle(augmented['image'],
-                  tuple(np.multiply(augmented['bboxes'][0][:2], [450, 450]).astype(int)),
-                  tuple(np.multiply(augmented['bboxes'][0][2:], [450, 450]).astype(int)),
-                  (255, 0, 0), 2)
-
-plt.imshow(augmented['image'])
-
-plt.show()
+        except Exception as e:
+            print(e)
