bc38ef0f6d87e7c3331b79bd8f3c393a4458f679
Sirdabpoop1 <adrian.pun27@stgeorges.bc.ca>
2025-05-30 14:05:14 -0700

    d

--- ai.py
+++ ai.py
@@ -21,19 +21,55 @@ for gpu in gpus:
 
 tf.config.list_physical_devices("GPU")
 
-images = tf.data.Dataset.list_files('data\\images\\*.jpg')
-
 def load_image(x):
     byte_img = tf.io.read_file(x)
     img = tf.io.decode_jpeg(byte_img)
     return img
 
-images = images.map(load_image)
+train_images = tf.data.Dataset.list_files('aug_data\\train\\images\\*.jpg', shuffle = False)
+train_images = train_images.map(load_image)
+train_images = train_images.map(lambda x: tf.image.resize(x, (120, 120)))
+train_images = train_images.map(lambda x: x/225)
 
-image_gen = images.batch(4).as_numpy_iterator()
-plot_images = image_gen.next()
+test_images = tf.data.Dataset.list_files('aug_data\\test\\images\\*.jpg', shuffle = False)
+test_images = test_images.map(load_image)
+test_images = test_images.map(lambda x: tf.image.resize(x, (120, 120)))
+test_images = test_images.map(lambda x: x/225)
 
-fig, ax = plt.subplots(ncols = 4, figsize = (20, 20))
-for idx, image in enumerate(plot_images):
-    ax[idx].imshow(image)
-plt.show()
+val_images = tf.data.Dataset.list_files('aug_data\\val\\images\\*.jpg', shuffle = False)
+val_images = val_images.map(load_image)
+val_images = val_images.map(lambda x: tf.image.resize(x, (120, 120)))
+val_images = val_images.map(lambda x: x/225)
+
+def load_labels(label_path):
+    with open(label_path.numpy(), 'r', encoding = "utf-8") as f:
+        label = json.load(f)
+    
+    return [label['class']], label['bbox']
+
+train_labels = tf.data.Dataset.list_files('aug_data\\train\\labels\\*.json', shuffle = False)
+train_labels = train_labels.map(lambda x: tf.py_function(load_labels, [x], (tf.uint8, tf.float16)))
+
+test_labels = tf.data.Dataset.list_files('aug_data\\test\\labels\\*.json', shuffle = False)
+test_labels = test_labels.map(lambda x: tf.py_function(load_labels, [x], (tf.uint8, tf.float16)))
+
+val_labels = tf.data.Dataset.list_files('aug_data\\val\\labels\\*.json', shuffle = False)
+val_labels = val_labels.map(lambda x: tf.py_function(load_labels, [x], (tf.uint8, tf.float16)))
+
+
+print(len(train_images), len(train_labels), len(test_images), len(test_labels), len(val_images), len(val_labels))
+
+train = tf.data.Dataset.zip((train_images, train_labels))
+train = train.shuffle(5000)
+train = train.batch(8)
+train = train.prefetch(4)
+
+test = tf.data.Dataset.zip((test_images, test_labels))
+test = test.shuffle(5000)
+test = test.batch(8)
+test = test.prefetch(4)
+
+val = tf.data.Dataset.zip((val_images, val_labels))
+val = val.shuffle(5000)
+val = val.batch(8)
+val = val.prefetch(4)
